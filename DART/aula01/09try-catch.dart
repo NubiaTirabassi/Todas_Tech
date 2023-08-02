9b6ec993db0f6e3211a7d4c27b9e6ccdea522f66
Nubia <nu.ragagnin91@gmail.com>
2023-08-02 16:48:08 -0300

    todas tech

---
+++ DART/aula01/09try-catch.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  // Tentativa
+  try{
+    int numero = int.parse("a2");
+    print("O número informadoe é $numero");
+  }catch(erro){
+    print("Falha ao executar: $erro");
+  }
+
+}
+
+
